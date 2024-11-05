20c5c8ca9805b1735bc9931da8cc2fd3a1d7cea9
Efim <efim.shapoval@mail.ru>
2024-11-05 05:59:29 -0800

    Initial Commit

---
+++ EfChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EfChat
+//
+//  Created by 1111 on 05.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EfChatTests/EfChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  EfChatTests.swift
+//  EfChatTests
+//
+//  Created by 1111 on 05.11.2024.
+//
+
+import Testing
+@testable import EfChat
+
+struct EfChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
