0b9748e266471cce80f30d30e349d6bbfd6121c5
Efim <efim.shapoval@mail.ru>
2024-11-06 08:13:38 -0800

    02

--- EfChat/ViewController.swift
+++ EfChat/ViewController.swift
@@ -7,12 +7,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Message {
+    let text: String
+    let sender: String
+    let timestamp: Date
+}
 
+class ViewController: UIViewController, UITableViewDataSource {
+   
+    // Массив для хранения сообщений
+    var messages: [Message] = []
+    
+    
+    // Ссылаемся на текстовое поле и таблицу
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Устанавливаем источник данных для таблицы
+        tableView.dataSource = self
+        
     }
+    
+    // Метод для отправки сообщения
+    @IBAction func sendMessage(_ sender: UIButton) {
+        // Проверяем, что текстовое поле не пустое
+        guard  let text = textField.text, !text.isEmpty else { return }
+        
+        // Создаем новое сообщение
+        let newMessage = Message(text: text, sender: "User", timestamp: Date())
+        
+        // Доблвляем сообщение в массив
+        messages.append(newMessage)
+        
+        // Обновляем таблицу, чтобы отобразить новое сообщение
+        tableView.reloadData()
+        
+        // Очищаем текстовое поле
+        textField.text = ""
+        
+        // Прокручиваем таблицу вниз чтобы увидеть новое сообщение
+        let IndexPath = IndexPath(row: messages.count - 1, section: 0)
+        tableView.scrollToRow(at: IndexPath, at: .bottom, animated: true)
+    }
+    
+    // MARK:  - UITableViewDataSource методы
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
+        
+        let messenger = messages[indexPath.row]
+        
+        // Настроим ячейку для отображения текста сообщения
+        cell.textLabel?.text = messenger.text
+        
+        return cell
+    }
+    
+    
 
 
 }
